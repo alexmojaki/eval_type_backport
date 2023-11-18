87a1744b4e9959755e5f9becf4e39dd25fc2212a
Alex Hall <alex.mojaki@gmail.com>
2023-11-18 15:06:57 +0200

    import version

--- eval_type_backport/__init__.py
+++ eval_type_backport/__init__.py
@@ -1,3 +1,9 @@
 from .eval_type_backport import eval_type_backport
 
-__all__ = ['eval_type_backport']
+try:
+    from .version import __version__
+except ImportError:
+    # version.py is auto-generated with the git tag when building
+    __version__ = '???'
+
+__all__ = ['eval_type_backport', '__version__']
