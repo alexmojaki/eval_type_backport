2d6f92e18cbed07f81d0a06cf99f937e8d1a6e17
Alex Hall <alex.mojaki@gmail.com>
2023-12-16 21:06:48 +0200

    Handle fussy typing._type_check

--- tests/test_eval_type_backport.py
+++ tests/test_eval_type_backport.py
@@ -92,6 +92,12 @@ def test_eval_type_backport():
             ]
         ],
     )
+    check_eval('t.Literal[1]', t.Literal[1])
+    check_eval('t.Literal[1] | t.Literal[2]', t.Union[t.Literal[1], t.Literal[2]])
+    check_eval(
+        't.List[t.Literal[1] | t.Literal[2]]',
+        t.List[t.Union[t.Literal[1], t.Literal[2]]],
+    )
 
 
 def test_other_type_error():
