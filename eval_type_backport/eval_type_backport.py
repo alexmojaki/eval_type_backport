c4a4d8662dbc3fe413472651e62f61fb85b61424
bswck <bartosz@ilikepython.com>
2025-11-13 03:53:11 +0100

    Mark `if TYPE_CHECKING` as uncovered

--- eval_type_backport/eval_type_backport.py
+++ eval_type_backport/eval_type_backport.py
@@ -11,7 +11,7 @@ import uuid
 from collections.abc import Mapping
 from typing import TYPE_CHECKING, Any
 
-if TYPE_CHECKING:
+if TYPE_CHECKING:  # pragma: no cover
     from _typeshed import Unused
 
 
