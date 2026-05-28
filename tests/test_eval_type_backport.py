c9f766bfa5cf62ad82f81f060ed0b0d09af3fd7e
Alex Hall <alex.mojaki@gmail.com>
2026-05-28 17:25:25 +0200

    restore direct subscript helper coverage

--- tests/test_eval_type_backport.py
+++ tests/test_eval_type_backport.py
@@ -10,7 +10,7 @@ import pytest
 
 import eval_type_backport as eval_type_backport_module
 from eval_type_backport import ForwardRef, eval_type_backport, install_patch
-from eval_type_backport.eval_type_backport import new_generic_types
+from eval_type_backport.eval_type_backport import new_generic_types, safe_subscript
 
 str((collections, contextlib, re))  # mark these as used (by eval calls)
 
@@ -381,6 +381,26 @@ def test_subscript_other_error():
             localns=locals(),
             try_default=False,
         )
+
+
+def test_safe_subscript():
+    value = object()
+    generic_types = t.cast(t.MutableMapping[t.Any, t.Any], new_generic_types)
+    generic_types[value] = t.List
+    try:
+        assert safe_subscript(value, int) == t.List[int]
+    finally:
+        del generic_types[value]
+
+    with pytest.raises(TypeError, match='subscriptable'):
+        safe_subscript(object(), int)
+
+    class OtherSubscriptError:
+        def __getitem__(self, item):
+            raise TypeError('other')
+
+    with pytest.raises(TypeError, match='other'):
+        safe_subscript(OtherSubscriptError(), int)
 
 
 def test_copy_forward_ref_attrs():
